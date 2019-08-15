f85660917d95a78a41c21ff3921d6d2b43c77fb5
shogo.ito <noonslugger@i.softbank.jp>
2019-08-15 04:33:53 +0000

    add column

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -8,13 +8,14 @@
         <table class="table table-striped">
             <thead>
                 <tr>
-                    <th>id</th><th>タスク</th>
+                    <th>id</th><th>ステイタス</th><th>タスク</th>
                 </tr>
             </thead>
             <tbody>
                 @foreach ($tasks as $task)
                 <tr>
                     <td>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!}</td>
+                    <td>{{ $task->status }}</td>
                     <td>{{ $task->content }}</td>
                 </tr>
                 @endforeach
