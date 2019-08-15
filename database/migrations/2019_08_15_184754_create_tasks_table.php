0c2a4e7e1779874089fcfac451dac846330597c1
shogo.ito <noonslugger@i.softbank.jp>
2019-08-15 10:43:03 +0000

    migration modification

--- database/migrations/2019_08_15_184754_create_tasks_table.php
+++ database/migrations/2019_08_15_184754_create_tasks_table.php
@@ -13,7 +13,7 @@ class CreateTasksTable extends Migration
      */
     public function up()
     {
-        Schema::create('messages', function (Blueprint $table) {
+        Schema::create('tasks', function (Blueprint $table) {
             $table->increments('id');
             $table->string('content');
             $table->timestamps();
@@ -27,6 +27,6 @@ class CreateTasksTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('messages');
+        Schema::dropIfExists('tasks');
     }
 }
